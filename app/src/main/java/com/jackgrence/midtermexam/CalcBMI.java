0005056bcba0a7484c6a8bccb84e4c9d77cac7e2
JackGrence <green1181772@gmail.com>
2018-04-14 21:48:47 +0800

    Complete calcBMI layout

--- app/src/main/java/com/jackgrence/midtermexam/CalcBMI.java
+++ app/src/main/java/com/jackgrence/midtermexam/CalcBMI.java
@@ -2,6 +2,7 @@ package com.jackgrence.midtermexam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CalcBMI extends AppCompatActivity
 {
@@ -11,5 +12,14 @@ public class CalcBMI extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc_bmi);
+    }
+
+    public void startCalc(View v)
+    {
+    }
+
+    public void exit_activity(View v)
+    {
+        finish();
     }
 }
